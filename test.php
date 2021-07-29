b2729f29af23da0ae8005828f4b31cf87563ef44
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2021-07-30 06:34:07 +0800

    初始化仓库

--- test.php
+++ test.php
@@ -19,6 +19,9 @@ if (isset($_GET['log']) && $_GET['log']) {
     }
     echo nl2br(file_get_contents(WEBPATH.'caches/error_log.php'));
     exit;
+} elseif (isset($_GET['phpinfo']) && $_GET['phpinfo']) {
+    phpinfo();
+    exit;
 }
 
 echo_msg(1, '客户端信息：'.$_SERVER['HTTP_USER_AGENT']);
@@ -27,7 +30,7 @@ echo_msg(1, '客户端信息：'.$_SERVER['HTTP_USER_AGENT']);
 if (version_compare(PHP_VERSION, '7.0.0') < 0) {
     exit("<font color=red>PHP版本必须在7.0及以上，当前".PHP_VERSION."</font>");
 } else {
-    echo_msg(1, 'PHP版本要求：7.0及以上，当前'.PHP_VERSION);
+    echo_msg(1, 'PHP版本要求：7.0及以上，当前'.PHP_VERSION.'，<a style="color:blue;text-decoration:none;" href="'.SELF.'?phpinfo=true">查看环境</a>');
 }
 
 // GD库判断
