51fc26c724fe6868763641121969a3ab51a5d273
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2021-08-09 19:07:35 +0800

    初始化仓库

--- cms/libs/functions/global.func.php
+++ cms/libs/functions/global.func.php
@@ -1810,6 +1810,73 @@ function get_memberinfo_buyusername($username, $field='') {
 	}
 }
 
+if (!function_exists('dr_letter_avatar')) {
+	/**
+	 * 首字母头像
+	 * @param $text
+	 * @return string
+	 */
+	function dr_letter_avatar($text) {
+		$total = unpack('L', hash('adler32', $text, true))[1];
+		$hue = $total % 360;
+		list($r, $g, $b) = dr_hsv2rgb($hue / 360, 0.3, 0.9);
+		$bg = "rgb({$r},{$g},{$b})";
+		$color = "#ffffff";
+		$first = mb_strtoupper(mb_substr($text, 0, 1));
+		$src = base64_encode('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="100" width="100"><rect fill="' . $bg . '" x="0" y="0" width="100" height="100"></rect><text x="50" y="50" font-size="50" text-copy="fast" fill="' . $color . '" text-anchor="middle" text-rights="admin" dominant-baseline="central">' . $first . '</text></svg>');
+		$value = 'data:image/svg+xml;base64,' . $src;
+		return $value;
+	}
+}
+
+if (!function_exists('dr_hsv2rgb')) {
+	function dr_hsv2rgb($h, $s, $v) {
+		$r = $g = $b = 0;
+		$i = floor($h * 6);
+		$f = $h * 6 - $i;
+		$p = $v * (1 - $s);
+		$q = $v * (1 - $f * $s);
+		$t = $v * (1 - (1 - $f) * $s);
+		switch ($i % 6) {
+			case 0:
+				$r = $v;
+				$g = $t;
+				$b = $p;
+				break;
+			case 1:
+				$r = $q;
+				$g = $v;
+				$b = $p;
+				break;
+			case 2:
+				$r = $p;
+				$g = $v;
+				$b = $t;
+				break;
+			case 3:
+				$r = $p;
+				$g = $q;
+				$b = $v;
+				break;
+			case 4:
+				$r = $t;
+				$g = $p;
+				$b = $v;
+				break;
+			case 5:
+				$r = $v;
+				$g = $p;
+				$b = $q;
+				break;
+		}
+		return array(
+			floor($r * 255),
+			floor($g * 255),
+			floor($b * 255)
+		);
+	}
+}
+
 /**
  * 获取用户头像
  * @param $uid 默认为userid
@@ -1829,7 +1896,7 @@ function get_memberavatar($uid, $size = '') {
 			$avatar = SYS_AVATAR_URL.$avatar_db['filepath'];
 		}
 	} else {
-		$avatar = IMG_PATH.'member/nophoto.gif';
+		$avatar = dr_letter_avatar($memberinfo['nickname']);
 	}
 	return $avatar;
 }
