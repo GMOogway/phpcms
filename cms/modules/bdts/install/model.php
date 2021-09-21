157a39be4e3db1531fd2abfaf6d1a1e0ab1e3a73
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2021-09-21 09:14:38 +0800

    初始化仓库

--- cms/modules/bdts/install/model.php
+++ cms/modules/bdts/install/model.php
@@ -1,8 +1,4 @@
 <?php  
 defined('IN_CMS') or exit('Access Denied');
 defined('INSTALL') or exit('Access Denied');
-<<<<<<< HEAD
-=======
-//return array('bdts');
->>>>>>> 3f92d8260bf3c0fa60c1915537939fce18f212ec
 ?>
