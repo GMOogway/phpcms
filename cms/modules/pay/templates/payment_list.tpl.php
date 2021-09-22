9447788f6813fdd79dabb59af36ae4a2a93ffedc
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2021-09-22 12:51:51 +0800

    初始化仓库

--- cms/modules/pay/templates/payment_list.tpl.php
+++ cms/modules/pay/templates/payment_list.tpl.php
@@ -8,12 +8,12 @@
     <table width="100%" cellspacing="0">
         <thead>
             <tr>
-            <th width="10%"  align="left"><?php echo L('payment_mode').L('name')?></th>
-            <th width="5%"><?php echo L('plus_version')?></th>
-            <th width="15%"><?php echo L('plus_author')?></th>
-            <th width="45%"><?php echo L('desc')?></th>
-             <th width="10%"><?php echo L('listorder')?></th>
-            <th width="15%"><?php echo L('operations_manage')?></th>
+            <th width="120"><?php echo L('payment_mode').L('name')?></th>
+            <th width="100"><?php echo L('plus_version')?></th>
+            <th width="120"><?php echo L('plus_author')?></th>
+            <th><?php echo L('desc')?></th>
+            <th width="80"><?php echo L('listorder')?></th>
+            <th width="100"><?php echo L('operations_manage')?></th>
             </tr>
         </thead>
     <tbody>
@@ -22,17 +22,17 @@ if(is_array($infos['data'])){
 	foreach($infos['data'] as $info){
 ?>   
 	<tr>
-	<td width="10%"><?php echo $info['pay_name']?></td>
-	<td  width="5%" align="center"><?php echo $info['version']?></td>
-	<td  width="15%" align="center"><?php echo $info['author']?></td>
-	<td width="45%" align="center"><?php echo $info['pay_desc']?></td>
-	<td width="10%" align="center"><?php echo $info['pay_order']?></td>
-	<td width="15%" align="center">
+	<td><?php echo $info['pay_name']?></td>
+	<td><?php echo $info['version']?></td>
+	<td><?php echo $info['author']?></td>
+	<td><?php echo $info['pay_desc']?></td>
+	<td><?php echo $info['pay_order']?></td>
+	<td>
 	<?php if ($info['enabled']) {?>
 	<a href="javascript:edit('<?php echo $info['pay_id']?>', '<?php echo $info['pay_name']?>')"><?php echo L('configure')?></a> | 
 	<a href="javascript:confirmurl('?m=pay&c=payment&a=delete&id=<?php echo $info['pay_id']?>', '<?php echo L('confirm',array('message'=>$info['pay_name']))?>')"><?php echo L('plus_uninstall')?></a>
 	<?php } else {?>
-	<a href="javascript:add('<?php echo $info['pay_code']?>', '<?php echo $info['pay_name']?>')"><?php echo L('plus_install')?></a>
+	<a class="btn btn-xs green" href="javascript:add('<?php echo $info['pay_code']?>', '<?php echo $info['pay_name']?>')"><?php echo L('plus_install')?></a>
 	<?php }?>
 	</td>
 	</tr>
