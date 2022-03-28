6b20ec5b0c84ff59c00e7e0f80f2668585e37552
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2022-03-28 19:19:02 +0800

    初始化仓库

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
 header('X-Frame-Options: SAMEORIGIN'); //防止被站外加入iframe中浏览
 
 // 是否是开发者模式
-define('IS_DEV', FALSE);
+define('IS_DEV', 1);
 
 // 后台管理标识
 !defined('IS_ADMIN') && define('IS_ADMIN', FALSE);
