a4fcf6d66874b2e958f763d20d1525e25961079d
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2022-04-14 12:31:23 +0800

    初始化仓库

--- caches/configs/version.php
+++ caches/configs/version.php
@@ -4,9 +4,9 @@ return array(
 'pc_version' => 'V9.6.3', //版本号
 'pc_release' => '20170515', //更新日期
 'cms_version' => 'V10.0.0', //cms 版本号
-'cms_release' => '20220316', //cms 更新日期
-'cms_updatetime' => '2022-03-16', // 服务端最近更新时间
-'cms_downtime' => '2022-03-16 16:06:06', // 本网站程序下载时间
+'cms_release' => '20220416', //cms 更新日期
+'cms_updatetime' => '2022-04-16', // 服务端最近更新时间
+'cms_downtime' => '2022-04-16 16:06:06', // 本网站程序下载时间
 'update' => '0', //cms 更新
 );
 ?>
