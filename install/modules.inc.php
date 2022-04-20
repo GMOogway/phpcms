947a47e220d7accd8faba8ba2a73f0d1a458126e
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2022-04-20 22:04:00 +0800

    初始化仓库

--- install/modules.inc.php
+++ install/modules.inc.php
@@ -2,7 +2,7 @@
 $CMS_VERSION_NAME = 'CMS V10';
 
 $CMS_MODULES = array(
-'name'=>array('announce','comment','link','vote','message','mood','poster','formguide','tag','','404','bdts','custom','customfield','fclient','guestbook','import','slider','sqltoolplus','taglist'),
+'name'=>array('announce','comment','link','vote','message','mood','poster','formguide','tag','404','bdts','custom','customfield','fclient','guestbook','import','slider','sqltoolplus','taglist'),
 'modulename'=>array('网站公告','评论','友情链接','投票','短消息','心情指数','广告管理','表单向导','标签向导','404错误页','百度推送','资料','变量','客户站群','留言板','外部导入','幻灯片','SQL工具','TAG管理'),
 'modules'=>array('announce'=>'网站公告','comment'=>'评论','link'=>'友情链接','vote'=>'投票','message'=>'短消息','mood'=>'心情指数','poster'=>'广告管理','formguide'=>'表单向导','tag'=>'标签向导','404'=>'404错误页','bdts'=>'百度推送','custom'=>'资料','customfield'=>'变量','fclient'=>'客户站群','guestbook'=>'留言板','import'=>'外部导入','slider'=>'幻灯片','sqltoolplus'=>'SQL工具','taglist'=>'TAG管理'),
 'introduce'=>array()
