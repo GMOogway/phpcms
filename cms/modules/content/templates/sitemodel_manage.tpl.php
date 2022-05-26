60e89bb473a3fc95fe5c5a66a015d9c38b428083
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2022-05-26 12:08:06 +0800

    初始化仓库

--- cms/modules/content/templates/sitemodel_manage.tpl.php
+++ cms/modules/content/templates/sitemodel_manage.tpl.php
@@ -12,11 +12,8 @@ include $this->admin_tpl('header','admin');?>
     <table width="100%" cellspacing="0">
         <thead>
             <tr class="heading">
-            <th width="100" class="<?php echo dr_sorting('modelid')?>" name="modelid">modelid</th>
-            <th width="180" class="<?php echo dr_sorting('name')?>" name="name"><?php echo L('model_name');?></th>
-            <th width="180" class="<?php echo dr_sorting('tablename')?>" name="tablename"><?php echo L('tablename');?></th>
-            <th class="<?php echo dr_sorting('description')?>" name="description"><?php echo L('description');?></th>
-            <th width="100" class="<?php echo dr_sorting('disabled')?>" name="disabled"><?php echo L('status');?></th>
+            <th width="50"><?php echo L('可用');?></th>
+            <th width="200"><?php echo L('model_name');?> / <?php echo L('tablename');?></th>
             <th width="150"><?php echo L('items');?></th>
             <th><?php echo L('operations_manage');?></th>
             </tr>
@@ -27,13 +24,15 @@ include $this->admin_tpl('header','admin');?>
         $tablename = $r['name'];
     ?>
     <tr>
-        <td align='center'><?php echo $r['modelid']?></td>
-        <td align='center'><?php echo $tablename?></td>
-        <td align='center'><?php echo $r['tablename']?></td>
-        <td><?php echo $r['description']?></td>
-        <td align='center'><?php echo $r['disabled'] ? L('icon_locked') : L('icon_unlock')?></td>
-        <td align='center'><?php echo $r['items']?></td>
-        <td align='center'><a class="btn btn-xs blue" href="?m=content&c=sitemodel_field&a=init&modelid=<?php echo $r['modelid']?>&menuid=<?php echo $this->input->get('menuid');?>"><?php echo L('field_manage');?></a><a class="btn btn-xs green" href="?m=content&c=sitemodel&a=edit&modelid=<?php echo $r['modelid']?>&menuid=<?php echo $this->input->get('menuid');?>"><?php echo L('edit');?></a><a class="btn btn-xs dark" href="?m=content&c=sitemodel&a=disabled&modelid=<?php echo $r['modelid']?>&menuid=<?php echo $this->input->get('menuid');?>"><?php echo $r['disabled'] ? L('field_enabled') : L('field_disabled');?></a><a class="btn btn-xs red" href="javascript:;" onclick="model_delete(this,'<?php echo $r['modelid']?>','<?php echo L('confirm_delete_model',array('message'=>new_addslashes($tablename)));?>','<?php echo $r['items']?>')"><?php echo L('delete')?></a><a class="btn btn-xs yellow" href="?m=content&c=sitemodel&a=export&modelid=<?php echo $r['modelid']?>&menuid=<?php echo $this->input->get('menuid');?>"><?php echo L('export');?></a></td>
+        <td><?php echo $r['disabled'] ? '<a href="?m=content&c=sitemodel&a=disabled&modelid='.$r['modelid'].'&menuid='.$this->input->get('menuid').'" class="badge badge-no"><i class="fa fa-times"></i></a>' : '<a href="?m=content&c=sitemodel&a=disabled&modelid='.$r['modelid'].'&menuid='.$this->input->get('menuid').'" class="badge badge-yes"><i class="fa fa-check"></i></a>';?></td>
+        <td><?php echo $tablename?> / <?php echo $r['tablename']?></td>
+        <td><?php echo $r['items']?></td>
+        <td>
+            <a class="btn btn-xs blue" href="?m=content&c=sitemodel_field&a=init&modelid=<?php echo $r['modelid']?>&menuid=<?php echo $this->input->get('menuid');?>"> <i class="fa fa-code"></i> <?php echo L('field_manage');?></a>
+            <a class="btn btn-xs green" href="?m=content&c=sitemodel&a=edit&modelid=<?php echo $r['modelid']?>&menuid=<?php echo $this->input->get('menuid');?>"> <i class="fa fa-edit"></i> <?php echo L('edit');?></a>
+            <a class="btn btn-xs red" href="javascript:;" onclick="model_delete(this,'<?php echo $r['modelid']?>','<?php echo L('confirm_delete_model',array('message'=>new_addslashes($tablename)));?>','<?php echo $r['items']?>')"> <i class="fa fa-trash"></i> <?php echo L('delete')?></a>
+            <a class="btn btn-xs yellow" href="?m=content&c=sitemodel&a=export&modelid=<?php echo $r['modelid']?>&menuid=<?php echo $this->input->get('menuid');?>"> <i class="fa fa-sign-out"></i> <?php echo L('export');?></a>
+        </td>
     </tr>
     <?php } ?>
     </tbody>
