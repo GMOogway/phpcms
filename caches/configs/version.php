9867cdb628014e7aa63036d6370f46b0525715e3
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2022-05-27 17:23:08 +0800

    初始化仓库

--- caches/configs/version.php
+++ caches/configs/version.php
@@ -4,9 +4,9 @@ return array(
 'pc_version' => 'V9.6.3', //版本号
 'pc_release' => '20170515', //更新日期
 'cms_version' => 'V10.0.0', //cms 版本号
-'cms_release' => '20220527', //cms 更新日期
-'cms_updatetime' => '2022-05-27', // 服务端最近更新时间
-'cms_downtime' => '2022-05-27 16:06:06', // 本网站程序下载时间
+'cms_release' => '20220528', //cms 更新日期
+'cms_updatetime' => '2022-05-28', // 服务端最近更新时间
+'cms_downtime' => '2022-05-28 16:06:06', // 本网站程序下载时间
 'update' => '0', //cms 更新
 );
 ?>
