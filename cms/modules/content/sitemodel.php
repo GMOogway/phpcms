7dbb3962b8847ef3f89ad5f5bb9149b2cfc9c065
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2022-06-21 20:29:01 +0800

    初始化仓库

--- cms/modules/content/sitemodel.php
+++ cms/modules/content/sitemodel.php
@@ -280,7 +280,7 @@ class sitemodel extends admin {
 			$modelinfoarr[$k] = $v;
 			$modelinfoarr[$k]['setting'] = string2array($v['setting']);
 		}
-		$res = var_export($modelinfoarr, TRUE);
+		$res = array2string($modelinfoarr);
 		header('Content-Disposition: attachment; filename="'.$modelarr[$modelid]['tablename'].'.model"');
 		exit($res);
 	}
@@ -376,7 +376,89 @@ class sitemodel extends admin {
 							if(isset($v['setting']['fieldtype'])) {
 								$field_type = $v['setting']['fieldtype'];
 							}
-							require MODEL_PATH.'add.sql.php';
+							$tips = $v['name'] ? ' COMMENT \''.$v['name'].'\'' : '';
+							$defaultvalue = isset($v['setting']['defaultvalue']) ? $v['setting']['defaultvalue'] : '';
+							//正整数 UNSIGNED && SIGNED
+							$minnumber = isset($v['setting']['minnumber']) ? $v['setting']['minnumber'] : 1;
+							$decimaldigits = isset($v['setting']['decimaldigits']) ? $v['setting']['decimaldigits'] : '';
+							switch($field_type) {
+								case 'varchar':
+									if(!$maxlength) $maxlength = 255;
+									$maxlength = min($maxlength, 255);
+									$sql = "ALTER TABLE `$tablename` ADD `$field` VARCHAR( $maxlength ) NOT NULL DEFAULT '$defaultvalue' $tips";
+									$this->db->query($sql);
+								break;
+
+								case 'tinyint':
+									if(!$maxlength) $maxlength = 3;
+									$minnumber = intval($minnumber);
+									$defaultvalue = intval($defaultvalue);
+									$sql = "ALTER TABLE `$tablename` ADD `$field` TINYINT( $maxlength ) ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue' $tips";
+									$this->db->query($sql);
+								break;
+								
+								case 'number':
+									$minnumber = intval($minnumber);
+									$defaultvalue = $decimaldigits == 0 ? intval($defaultvalue) : floatval($defaultvalue);
+									$sql = "ALTER TABLE `$tablename` ADD `$field` ".($decimaldigits == 0 ? 'INT' : 'FLOAT')." ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue' $tips";
+									$this->db->query($sql);
+								break;
+
+								case 'smallint':
+									$minnumber = intval($minnumber);
+									$sql = "ALTER TABLE `$tablename` ADD `$field` SMALLINT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL $tips";
+									$this->db->query($sql);
+								break;
+
+								case 'int':
+									$minnumber = intval($minnumber);
+									$defaultvalue = intval($defaultvalue);
+									$sql = "ALTER TABLE `$tablename` ADD `$field` INT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue' $tips";
+									$this->db->query($sql);
+								break;
+
+								case 'mediumint':
+									$minnumber = intval($minnumber);
+									$defaultvalue = intval($defaultvalue);
+									$sql = "ALTER TABLE `$tablename` ADD `$field` INT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue' $tips";
+									$this->db->query($sql);
+								break;
+
+								case 'mediumtext':
+									$sql = "ALTER TABLE `$tablename` ADD `$field` MEDIUMTEXT NOT NULL $tips";
+									$this->db->query($sql);
+								break;
+								
+								case 'text':
+									$sql = "ALTER TABLE `$tablename` ADD `$field` TEXT NOT NULL $tips";
+									$this->db->query($sql);
+								break;
+
+								case 'date':
+									$sql = "ALTER TABLE `$tablename` ADD `$field` DATE NULL $tips";
+									$this->db->query($sql);
+								break;
+								
+								case 'datetime':
+									$sql = "ALTER TABLE `$tablename` ADD `$field` DATETIME NULL $tips";
+									$this->db->query($sql);
+								break;
+								
+								case 'timestamp':
+									$sql = "ALTER TABLE `$tablename` ADD `$field` TIMESTAMP NOT NULL $tips";
+									$this->db->query($sql);
+								break;
+								//特殊自定义字段
+								case 'pages':
+									$this->db->query("ALTER TABLE `$tablename` ADD `paginationtype` TINYINT( 1 ) NOT NULL DEFAULT '0' $tips");
+									$this->db->query("ALTER TABLE `$tablename` ADD `maxcharperpage` MEDIUMINT( 6 ) NOT NULL DEFAULT '0' $tips");
+								break;
+								case 'readpoint':
+									$defaultvalue = intval($defaultvalue);
+									$this->db->query("ALTER TABLE `$tablename` ADD `readpoint` smallint(5) unsigned NOT NULL default '$defaultvalue' $tips");
+									$this->db->query("ALTER TABLE `$tablename` ADD `paytype` tinyint(1) unsigned NOT NULL default '0' $tips");
+								break;
+							}
 							$v['setting'] = array2string($v['setting']);
 							$v['modelid'] = $modelid;
 							$v['siteid'] = $this->siteid;
