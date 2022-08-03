9e6dc487dd3db4bfd31bee29b7f62f49cf74fa27
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2022-08-03 12:50:11 +0800

    初始化仓库

--- cms/modules/content/templates/content_page.tpl.php
+++ cms/modules/content/templates/content_page.tpl.php
@@ -12,10 +12,21 @@ $menu_data = $this->menu_db->get_one(array('name' => 'category_manage', 'm' => '
         <div class="page-content page-content3 mybody-nheader main-content  ">
 <div class="page-bar">
     <ul class="page-breadcrumb">
+        <?php if(is_mobile(0)) {?>
+        <li class="dropdown"> <a href="javascript:location.reload(true);" class="on"> <i class="fa fa-list"></i> <?php echo L('page_manage');?></a> <a class="dropdown-toggle on" data-toggle="dropdown" data-hover="dropdown" data-close-others="true" aria-expanded="false"><i class="fa fa-angle-double-down"></i></a>
+            <ul class="dropdown-menu">
+                <li><a href="<?php if(strpos($category['url'],'http://')===false && strpos($category['url'],'https://') ===false) echo siteurl($this->siteid);echo $category['url'];?>" target="_blank"> <i class="fa fa-home"></i> <?php echo L('click_vistor');?> </a></li>
+                <li><a href="?m=content&c=sitemodel_field&a=init&modelid=-2&menuid=<?php echo $menu_data['id'];?>"> <i class="fa fa-code"></i> <?php echo L('page_field_manage');?> </a></li>
+                <li class="divider"></li>
+                <li><a href="?m=block&c=block_admin&a=public_visualization&catid=<?php echo $catid;?>&type=page"> <i class="fa fa-table"></i> <?php echo L('visualization_edit');?> </a></li>
+            </ul>
+        </li>
+        <?php } else {?>
         <li> <a href="javascript:location.reload(true);" class="on"> <i class="fa fa-list"></i> <?php echo L('page_manage');?></a> <i class="fa fa-circle"></i> </li>
         <li> <a href="<?php if(strpos($category['url'],'http://')===false && strpos($category['url'],'https://') ===false) echo siteurl($this->siteid);echo $category['url'];?>" target="_blank"> <i class="fa fa-home"></i> <?php echo L('click_vistor');?></a> <i class="fa fa-circle"></i> </li>
-        <li> <a href="?m=block&c=block_admin&a=public_visualization&catid=<?php echo $catid;?>&type=page"> <i class="fa fa-table"></i> <?php echo L('visualization_edit');?></a> <i class="fa fa-circle"></i> </li>
-        <li> <a href="?m=content&c=sitemodel_field&a=init&modelid=-2&menuid=<?php echo $menu_data['id'];?>&pc_hash=<?php echo dr_get_csrf_token();?>"> <i class="fa fa-code"></i> <?php echo L('page_field_manage');?></a> </li>
+        <li> <a href="?m=content&c=sitemodel_field&a=init&modelid=-2&menuid=<?php echo $menu_data['id'];?>"> <i class="fa fa-code"></i> <?php echo L('page_field_manage');?></a> <i class="fa fa-circle"></i> </li>
+        <li> <a href="?m=block&c=block_admin&a=public_visualization&catid=<?php echo $catid;?>&type=page"> <i class="fa fa-table"></i> <?php echo L('visualization_edit');?></a> </li>
+        <?php }?>
     </ul>
 </div>
 <form name="myform" id="myform" action="?m=content&c=content&a=add" class="form-horizontal" method="post" enctype="multipart/form-data">
