0d3eb64484bc112b5d23fc35de6539f87d5c408d
zhaoxunzhiyin <zhaoxunzhiyin@163.com>
2022-12-23 13:12:31 +0800

    初始化仓库

--- cms/libs/functions/global.func.php
+++ cms/libs/functions/global.func.php
@@ -4473,7 +4473,6 @@ function dr_catcher_data($url, $timeout = 0, $is_log = true, $ct = 0) {
 			));
 			curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 		}
-		///
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1 );
